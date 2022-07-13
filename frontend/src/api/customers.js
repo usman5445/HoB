5fbc2590d45acd00123963f682e975b8ed2f6bfa
usman <usmangavandi5445@gmail.com>
2022-07-13 19:56:50 +0530

    added alert and loading  in registration page. fixed price issue in product page

--- frontend/src/api/customers.js
+++ frontend/src/api/customers.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-export function registerCust(data) {
-  axios
-    .post("/customer", data)
-    .then((resp) => console.log(resp))
-    .catch((err) => console.error(err.response.data));
+export async function registerCust(data) {
+  return await axios.post("/customer", data);
 }
